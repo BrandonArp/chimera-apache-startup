90783eeca926013a2cd5c1c8e70a224de62e7151
Brandon Arp <brandon@arpnetworking.com>
2011-11-21 21:02:26 -0800

    added creating the run and lock directories

--- usr/lib/chimera/command/activateadmin/300-start-apache.rb
+++ usr/lib/chimera/command/activateadmin/300-start-apache.rb
@@ -9,7 +9,9 @@ ENV["APACHE_HTTPD"] = root + "/usr/sbin/apache2"
 ENV["APACHE_LOG_DIR"] = root + "/var/output/logs"
 `mkdir -p #{ENV["APACHE_LOG_DIR"]}`
 ENV["APACHE_RUN_DIR"] = root + "/var/run/apache2"
+`mkdir -p #{ENV["APACHE_RUN_DIR"]}`
 ENV["APACHE_LOCK_DIR"] = root + "/var/lock/apache2"
+`mkdir -p #{ENV["APACHE_LOCK_DIR"]}`
 ENV["APACHE_ENVVARS"] = "/dev/null"
 ENV["APACHE_RUN_USER"] = "www-data"
 ENV["APACHE_RUN_GROUP"] = "www-data"
